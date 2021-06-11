857a24fc38f611e76e1ecc758c559873ed954680
RyzekNoavek <adhith.chand@gmail.com>
2021-06-11 08:13:50 +0530

    Attempt 2

---
+++ _nuxt/static/1623379378/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623379378",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cb4fc53f"}}}}(null,"\u002F"));
