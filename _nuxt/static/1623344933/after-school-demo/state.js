a4810e17ba4017e294cd0aa369f22a1d1430605f
Adhith <46568705+RyzekNoavek@users.noreply.github.com>
2021-06-11 08:16:58 +0530

    Attempt 2 (#1)

--- _nuxt/static/1623344933/after-school-demo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623344933",layout:"default",error:a,serverRendered:true,routePath:"\u002Fafter-school-demo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4c6ee145"}}}}(null));
