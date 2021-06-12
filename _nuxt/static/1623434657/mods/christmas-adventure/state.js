c55f4db3399894269cdf17f9ccfaaedcf01a3a16
dokimods <adhith.chand@gmail.com>
2021-06-12 23:46:56 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e7802ecfc146d56d953063da6cebbfb3851b54cf

--- _nuxt/static/1623434657/mods/christmas-adventure/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623434657",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fchristmas-adventure",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b85dd770"}}}}(null));
