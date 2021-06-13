ce315dce5d8baff6d27f7276d6ff258f119128ca
dokimods <adhith.chand@gmail.com>
2021-06-13 15:50:43 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/56956f93feac118972db0ad6a011bf2ea58e0663

--- _nuxt/static/1623541611/mods/a-date-with-sayori/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623541611",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fa-date-with-sayori",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c607bb7b"}}}}(null));
