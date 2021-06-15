1e171eba14d8aec5b3a3bc9bad59c13bc858ff04
dokimods <adhith.chand@gmail.com>
2021-06-15 15:29:35 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/43099f34944dc0eb4853d1c65ba83b4e55659977

---
+++ _nuxt/static/1623770967/mods/48hr/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623770967",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002F48hr",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"46a31617"}}}}(null));
