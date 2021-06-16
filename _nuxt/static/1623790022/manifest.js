5fb86c847cc80a48ef5cb5711d658b1caa866b91
dokimods <adhith.chand@gmail.com>
2021-06-16 02:57:57 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/c082441235976a37e0af786a5aaff5e705b0e3b0

--- _nuxt/static/1623790022/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Finstallation-guide","\u002Fabout-us","\u002Fmods","\u002Fmods\u002F2nd-anniversary","\u002Fmods\u002F48hr","\u002Fmods\u002Fa-brand-new-day","\u002Fmods\u002Fa-date-with-sayori","\u002Fmods\u002Fa-fathers-redemption","\u002Fmods\u002Fa-lost-chapter"]})
--- _nuxt/static/1623790022/mods/a-lost-chapter/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623790022",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fa-lost-chapter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ce8ba75"}}}}(null));
