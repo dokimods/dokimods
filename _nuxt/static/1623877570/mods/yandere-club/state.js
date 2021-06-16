9b3b1977e89e2b26a36c513e0a508dac755fe590
dokimods <adhith.chand@gmail.com>
2021-06-16 23:31:22 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/d40ddf0543fb61810552f6a7ac11a3d3a0e30fc8

--- _nuxt/static/1623877570/mods/yandere-club/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623877570",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fyandere-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d50f54c4"}}}}(null));
