508f74ccc233c4aed939e3cb71fb919f6d3c32b5
dokimods <adhith.chand@gmail.com>
2021-06-16 18:52:51 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/d9449368b170e60031646e1cb7e1da3e81b15c91

---
+++ _nuxt/static/1623869566/mods/time-to-be-an-epic-hero/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623869566",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Ftime-to-be-an-epic-hero",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"60a20484"}}}}(null));
