07ecf88b205d6b3c6bbae92992e2200746e24e7d
dokimods <adhith.chand@gmail.com>
2021-06-16 03:18:07 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/d97b18474f67a58c7b44c352120732a30608a284

--- _nuxt/static/1623812265/mods/somnium/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623812265",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fsomnium",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1157374b"}}}}(null));
