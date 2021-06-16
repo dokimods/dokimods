23e366ea9b81e91113f138b09403f18741875f82
dokimods <adhith.chand@gmail.com>
2021-06-16 10:08:49 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/1718945cdbe7e755a6fa34eec37d113305b90521

---
+++ _nuxt/static/1623838123/mods/the-binding-of-isaac-mod/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623838123",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fthe-binding-of-isaac-mod",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f17b31a8"}}}}(null));
