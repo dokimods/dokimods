250b45b1c61e1aebc5c8056d6931a46e4eb3e97f
dokimods <adhith.chand@gmail.com>
2021-06-16 07:21:11 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/d40d0f12f8a730c8184c96ecfa581055f3a3e5ea

---
+++ _nuxt/static/1623828061/mods/the-yuri-parable/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623828061",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fthe-yuri-parable",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3b01b86f"}}}}(null));
