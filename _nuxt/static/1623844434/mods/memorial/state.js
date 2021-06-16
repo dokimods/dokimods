e2d1160c8e34f5c74a756d7c61ba9d7f730c1352
dokimods <adhith.chand@gmail.com>
2021-06-16 12:09:51 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/4743aa170e577946738ad948a8ccea029897992c

--- _nuxt/static/1623844434/mods/memorial/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623844434",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fmemorial",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e8130149"}}}}(null));
