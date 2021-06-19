8c446e399d11d0da6a2eb29b6d46f2bd54aad1f0
dokimods <adhith.chand@gmail.com>
2021-06-19 12:09:23 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f622ae1070d92dd1b65c1ea4e60764d6530e57fe

--- _nuxt/static/1624097449/mods/the-blood-moon-chapters-chapter-1--before-during-and-after-the-stories/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624097449",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fthe-blood-moon-chapters-chapter-1--before-during-and-after-the-stories",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a083f09c"}}}}(null));
--- _nuxt/static/1624097449/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624097449",layout:"front-page",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a083f09c"}}}}(null,"\u002F"));
