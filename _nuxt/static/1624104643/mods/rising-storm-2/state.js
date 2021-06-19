2d26a8c9fce63bd6ecf9aca787e0f3af5553a189
dokimods <adhith.chand@gmail.com>
2021-06-19 12:10:51 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/fc71d813dbb7f72308d0166203303ffb43cf0f92

---
+++ _nuxt/static/1624104643/mods/rising-storm-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624104643",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Frising-storm-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"683f4859"}}}}(null));
