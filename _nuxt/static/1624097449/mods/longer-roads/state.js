e934daebf0034ae11d3f1d7421592183899bab31
dokimods <adhith.chand@gmail.com>
2021-06-19 10:10:59 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/739603c19b5c1e61b139067561a6eb8002a42eec

---
+++ _nuxt/static/1624097449/mods/longer-roads/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624097449",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Flonger-roads",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a083f09c"}}}}(null));
