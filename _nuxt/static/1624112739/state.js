5c200e2dd93bcb5be17a17c0ff019ad6a75cdd2a
dokimods <adhith.chand@gmail.com>
2021-06-20 12:36:07 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/6140dd45c93566454a68b61a283623f0b32682c8

--- _nuxt/static/1624112739/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624112739",layout:"front-page",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"66d4ed0d"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1624192560/mods/bad-memories/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624192560",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fbad-memories",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6265c794"}}}}(null));
