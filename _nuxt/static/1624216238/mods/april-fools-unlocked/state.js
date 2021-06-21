8997cced6ee14ac68e6c2c2409146084d1eec2e3
dokimods <adhith.chand@gmail.com>
2021-06-21 03:52:23 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/af2a6712f9b4101db9cd4e8581e3d3bdba2bcc5b

--- _nuxt/static/1624216238/mods/april-fools-unlocked/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624216238",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fapril-fools-unlocked",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9bc5f9b9"}}}}(null));
---
+++ _nuxt/static/1624247534/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624247534",layout:"front-page",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"02d7bfe2"}}}}(null,"\u002F"));
