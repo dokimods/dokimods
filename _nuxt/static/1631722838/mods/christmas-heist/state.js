fc2be941cb15441ebdd0658d0b6e28ebc15613b4
dokimods <adhith.chand@gmail.com>
2021-10-08 21:58:40 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/2a0db7fa360e6e7b44181ea550a18e7747ab55f3

--- _nuxt/static/1631722838/mods/christmas-heist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631722838",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmods\u002Fchristmas-heist",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"289fcb33"}}}}(null));
