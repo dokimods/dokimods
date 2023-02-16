105c5a220ca6b2a5e0f960134322c5c9abd60b6e
dokimods <adhith.chand@gmail.com>
2023-02-16 10:17:13 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e07a0c34b8d3c480f29b64253f14a8d4e11fcd90

--- _nuxt/static/1676060245/mods/youtuber-club/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1676060245",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fyoutuber-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e4f0ef6e"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
