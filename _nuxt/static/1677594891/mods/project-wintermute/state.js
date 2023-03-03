b6bdfa6341aeffdb16e1141aecaf520a8d08b16d
dokimods <adhith.chand@gmail.com>
2023-03-03 19:55:42 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e07a0c34b8d3c480f29b64253f14a8d4e11fcd90

--- _nuxt/static/1677594891/mods/project-wintermute/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677594891",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fproject-wintermute",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"541d814c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
--- _nuxt/static/1677594891/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677594891",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"541d814c"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
