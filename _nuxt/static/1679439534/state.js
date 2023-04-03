27fe3394c51c28a27cbf8a84b890eac067ba4db5
dokimods <adhith.chand@gmail.com>
2023-04-03 07:15:44 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e07a0c34b8d3c480f29b64253f14a8d4e11fcd90

--- _nuxt/static/1679439534/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679439534",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f5cbf3fb"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
---
+++ _nuxt/static/1680506114/mods/how-to-make-a-ddlc-mod/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680506114",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fhow-to-make-a-ddlc-mod",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b356d0c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
