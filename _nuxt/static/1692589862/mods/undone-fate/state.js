cbfc8bed07da399825cb02452bdfe93caf2d8725
dokimods <adhith.chand@gmail.com>
2023-08-24 22:57:48 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e07a0c34b8d3c480f29b64253f14a8d4e11fcd90

--- _nuxt/static/1692589862/mods/undone-fate/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692589862",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fundone-fate",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"793e7838"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
