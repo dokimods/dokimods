23c819b021cb3b57b7c11473615260a7c5085e13
dokimods <adhith.chand@gmail.com>
2023-10-26 04:12:03 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/e07a0c34b8d3c480f29b64253f14a8d4e11fcd90

--- _nuxt/static/1692917837/mods/i-want-more-sayori/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1692917837",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fi-want-more-sayori",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0cdd8aa2"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
