96aa0488eabd7ded55debfc558923d0488508073
dokimods <adhith.chand@gmail.com>
2023-12-07 19:08:47 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

---
+++ _nuxt/static/1701976101/mods/the-last-sky/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701976101",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fthe-last-sky",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"00ec2e9a"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
