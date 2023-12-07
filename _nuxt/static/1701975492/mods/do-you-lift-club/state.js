bbad10e17494fac5336a8983a8ff58c6a358bdef
dokimods <adhith.chand@gmail.com>
2023-12-07 18:58:40 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

---
+++ _nuxt/static/1701975492/mods/do-you-lift-club/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701975492",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fdo-you-lift-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bd227431"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
