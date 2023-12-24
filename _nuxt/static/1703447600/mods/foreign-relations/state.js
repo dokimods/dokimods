8816c80becfa5a0be68c8c3e9f470c365cbe6954
dokimods <adhith.chand@gmail.com>
2023-12-24 19:53:49 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

---
+++ _nuxt/static/1703447600/mods/foreign-relations/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1703447600",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fforeign-relations",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40afbe3c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
