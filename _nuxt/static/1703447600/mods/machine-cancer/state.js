1663534c5c96ff67eec821f10e0fd398e7428682
dokimods <adhith.chand@gmail.com>
2023-12-27 15:52:06 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

--- _nuxt/static/1703447600/mods/machine-cancer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1703447600",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fmachine-cancer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"40afbe3c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
