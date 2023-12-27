be84fd07edfbd82163c78569836facd7b31f49dc
dokimods <adhith.chand@gmail.com>
2023-12-27 16:07:55 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

--- _nuxt/static/1703692292/mods/starry-eyed/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1703692292",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fstarry-eyed",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"248be941"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
