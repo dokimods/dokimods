cf6055baa9d7a15306f59c521f7e16a6a39e6f91
dokimods <adhith.chand@gmail.com>
2023-12-31 05:20:07 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

--- _nuxt/static/1703693246/mods/art-club/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1703693246",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fart-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"13f7d683"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
