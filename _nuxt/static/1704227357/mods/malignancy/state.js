e780b1f74b4d3b5a21b7181d6b33d5974ebc6c40
dokimods <adhith.chand@gmail.com>
2024-01-02 20:29:52 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

---
+++ _nuxt/static/1704227357/mods/malignancy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1704227357",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fmalignancy",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"35820b3a"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
