e0c30abd6c16d92b87df41797a547740e36bcb59
dokimods <adhith.chand@gmail.com>
2024-01-04 18:30:35 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/f2453cdd2683c0898259e7969b8989f7d011fe44

---
+++ _nuxt/static/1704393001/mods/dreams-of-literature-noir/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1704393001",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fdreams-of-literature-noir",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"64b9f10c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
