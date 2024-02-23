127822cf983c1611de2531166f5a144b988855e1
dokimods <adhith.chand@gmail.com>
2024-02-23 09:15:39 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

---
+++ _nuxt/static/1708679695/mods/i-am-monika/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708679695",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fi-am-monika",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"94ffe9de"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
