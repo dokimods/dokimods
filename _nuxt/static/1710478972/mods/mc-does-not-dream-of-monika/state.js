f200248883635b4f2b580d5f9578ff8920f58eda
dokimods <adhith.chand@gmail.com>
2024-03-16 05:10:28 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

--- _nuxt/static/1710478972/mods/mc-does-not-dream-of-monika/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710478972",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fmc-does-not-dream-of-monika",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"67a5ccac"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1710565791/mods/total-fold/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710565791",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Ftotal-fold",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d859ba6f"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
