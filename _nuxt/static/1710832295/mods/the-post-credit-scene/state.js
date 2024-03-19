25f057dc814fbc83d3534e355e5264912cf4764d
dokimods <adhith.chand@gmail.com>
2024-03-19 07:18:30 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

--- _nuxt/static/1710832295/mods/the-post-credit-scene/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710832295",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fthe-post-credit-scene",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6023d6cd"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
