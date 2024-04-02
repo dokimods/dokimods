47ba30ed3c18b55b517e885555aec9df74bc620b
dokimods <adhith.chand@gmail.com>
2024-04-02 21:08:18 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

---
+++ _nuxt/static/1712092055/mods/jumbled/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712092055",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fjumbled",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2b256db6"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
