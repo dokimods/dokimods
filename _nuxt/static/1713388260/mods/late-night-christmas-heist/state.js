fc96ae94261e2c2ccd8196f37a75b8f43993d8d5
dokimods <adhith.chand@gmail.com>
2024-04-18 18:32:56 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

--- _nuxt/static/1713388260/mods/late-night-christmas-heist/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713388260",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Flate-night-christmas-heist",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4d640593"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
