41846d67b460283f7639caadb267fd81b91b29a2
dokimods <adhith.chand@gmail.com>
2024-04-23 14:04:40 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

--- _nuxt/static/1713465136/mods/im-alive/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713465136",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fim-alive",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b3a0a1aa"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
