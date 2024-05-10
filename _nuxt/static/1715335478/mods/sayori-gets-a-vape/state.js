5e5f52a7f8c7ddb457183d136fe9232060102418
dokimods <adhith.chand@gmail.com>
2024-05-10 10:05:20 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

---
+++ _nuxt/static/1715335478/mods/sayori-gets-a-vape/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1715335478",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fsayori-gets-a-vape",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59664afc"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
