3f786a63d265a65a41de03cba221e165de20ae8b
dokimods <adhith.chand@gmail.com>
2024-05-17 10:25:48 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/17e4b80167fe7d62863e48b405f3a53d590dcc28

---
+++ _nuxt/static/1715941503/mods/forward-momentum/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1715941503",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fforward-momentum",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1bc7a5fa"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
