b22101093fbc686462dc2cf6fbb009f25e25dd8c
dokimods <adhith.chand@gmail.com>
2024-07-06 03:11:38 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

--- _nuxt/static/1718938564/mods/fake-reality/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1718938564",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Ffake-reality",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a75d17db"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
