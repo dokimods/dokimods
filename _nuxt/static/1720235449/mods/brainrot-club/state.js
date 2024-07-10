3bf73620e6c27b6e954ceffbd10082a3e551528b
dokimods <adhith.chand@gmail.com>
2024-07-10 01:23:15 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

--- _nuxt/static/1720235449/mods/brainrot-club/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1720235449",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fbrainrot-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ce1cb62a"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
