65c4bcc6094b38fd62b09376bc4f5d4f0320265d
dokimods <adhith.chand@gmail.com>
2024-08-13 02:29:21 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

--- _nuxt/static/1723513266/mods/yuri-and-the-wine/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1723513266",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fyuri-and-the-wine",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"dc30fc05"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1723516131/mods/cardboard-box/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1723516131",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fcardboard-box",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f2d4eaab"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
