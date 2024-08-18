229216188d8ee75eb7eaed285b28ec0166c3e799
dokimods <adhith.chand@gmail.com>
2024-08-18 10:25:19 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

---
+++ _nuxt/static/1723976689/mods/window-to-your-heart/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1723976689",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fwindow-to-your-heart",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2c7917ee"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
