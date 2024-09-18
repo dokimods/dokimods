9fb4f5c16f188285271304e46a7f2a35a7254f12
dokimods <adhith.chand@gmail.com>
2024-09-18 23:22:09 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

---
+++ _nuxt/static/1726701700/mods/double-trouple-adu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1726701700",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fdouble-trouple-adu",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1ec93803"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
