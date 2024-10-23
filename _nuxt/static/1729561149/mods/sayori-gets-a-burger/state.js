18a5d02e5febf57a6cb99d13d31643b1e94a1ef2
dokimods <adhith.chand@gmail.com>
2024-10-23 17:28:30 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

--- _nuxt/static/1729561149/mods/sayori-gets-a-burger/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1729561149",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fsayori-gets-a-burger",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ecfcb05e"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1729704474/mods/thot-club/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1729704474",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fthot-club",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f12e9248"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
