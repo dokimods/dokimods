ea3dd443b77d1e291d883adbe7c05666f7ed6e39
dokimods <adhith.chand@gmail.com>
2025-02-21 04:40:16 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

---
+++ _nuxt/static/1740112771/mods/acclamation/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1740112771",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Facclamation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1f7a992c"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
