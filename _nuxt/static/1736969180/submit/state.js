5539da2f5e1d41a4727bb7526e1ea78c9a09cbaf
dokimods <adhith.chand@gmail.com>
2025-02-21 04:27:13 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/9b010881121fddc72105ea276378ee862247e978

--- _nuxt/static/1736969180/submit/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1736969180",layout:"default",error:a,serverRendered:b,routePath:"\u002Fsubmit",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3831f943"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1740111990/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1740111990",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f41070fb"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
