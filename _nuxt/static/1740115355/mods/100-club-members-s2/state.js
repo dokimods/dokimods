b74db855d9d4447287e7d63a5912a958aa88025c
dokimods <adhith.chand@gmail.com>
2025-05-16 16:49:01 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/125615209fc2744deed9b6e7b08436ac59141d3a

--- _nuxt/static/1740115355/mods/100-club-members-s2/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1740115355",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002F100-club-members-s2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6dc8770d"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1747414095/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1747414095",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ae348cba"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
