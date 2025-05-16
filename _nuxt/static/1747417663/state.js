648d4c543cce8c0db1716a9d9aa0bf51d4d99142
dokimods <adhith.chand@gmail.com>
2025-05-16 17:48:29 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/125615209fc2744deed9b6e7b08436ac59141d3a

---
+++ _nuxt/static/1747417663/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1747417663",layout:"default",error:a,serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5c528bb8"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
