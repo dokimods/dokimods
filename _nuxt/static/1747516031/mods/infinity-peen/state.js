ada6ed38038b3e91ba482c46dd9228f269335ffa
dokimods <adhith.chand@gmail.com>
2025-05-17 21:08:02 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/125615209fc2744deed9b6e7b08436ac59141d3a

---
+++ _nuxt/static/1747516031/mods/infinity-peen/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1747516031",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Finfinity-peen",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"95374c68"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
