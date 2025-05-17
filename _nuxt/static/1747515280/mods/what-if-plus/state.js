7f44c8c7ec2b0887d54ee24aff80b306ad566dd0
dokimods <adhith.chand@gmail.com>
2025-05-17 20:55:29 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/125615209fc2744deed9b6e7b08436ac59141d3a

---
+++ _nuxt/static/1747515280/mods/what-if-plus/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1747515280",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fwhat-if-plus",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8a0995e3"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
