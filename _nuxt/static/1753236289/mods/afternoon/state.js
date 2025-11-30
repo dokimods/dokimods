9749c04fb792f05134e6ed33d3dbffc005cd9553
dokimods <adhith.chand@gmail.com>
2025-11-30 23:21:52 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/125615209fc2744deed9b6e7b08436ac59141d3a

--- _nuxt/static/1753236289/mods/afternoon/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1753236289",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fafternoon",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b731673"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1764544853/mods/god-syndrome/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1764544853",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fgod-syndrome",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6a75c74f"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
