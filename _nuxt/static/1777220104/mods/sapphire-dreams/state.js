14c7abbfde87f3e141b11bb0294075c8142f5176
dokimods <adhith.chand@gmail.com>
2026-04-26 16:25:40 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/761293013b8f3ed5bb59283d74794e1a5eb89545

--- _nuxt/static/1777220104/mods/sapphire-dreams/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1777220104",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fsapphire-dreams",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"24d7fc83"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
