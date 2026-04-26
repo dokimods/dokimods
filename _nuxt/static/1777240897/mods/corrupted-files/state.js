f84b696b5ed66319ecb252f56f3963b0685565ee
dokimods <adhith.chand@gmail.com>
2026-04-26 22:22:38 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/7774b5b7b08d62e6897ce665b2719e397a5aab82

--- _nuxt/static/1777240897/mods/corrupted-files/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1777240897",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fcorrupted-files",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"84f5ab5f"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
