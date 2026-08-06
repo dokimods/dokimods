f3c974a027e1712b3c2210f5ae2f4303ef2faf31
dokimods <adhith.chand@gmail.com>
2026-08-06 23:02:24 +0000

    Update from https://github.com/dokimods/dokimods-source/commit/2d24fab95bbd03f0da8dfe7b39bb8c20b6723f11

--- _nuxt/static/1786056303/mods/sunny-rains/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1786056303",layout:"default",error:a,serverRendered:b,routePath:"\u002Fmods\u002Fsunny-rains",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4734c575"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
